499250ed10d02c320bca7406cf64aae4ab0bd322
Your mostafafekry8 <m13527341@gmail.com>
2024-07-03 12:47:26 +0300

    first commit

--- js/Ui.js
+++ js/Ui.js
@@ -1,47 +1,43 @@
 export class Ui {
-    displayDataGame(data) {
-       let gamesBox = ``;
-       for (let i = 0; i < data.length; i++) {
-          gamesBox += `
-          <div class="col">
-          <div data-id="${data[i].id}"  class="card h-100 bg-transparent" role="button" ">
-             <div  class="card-body">
-                <figure class="position-relative">
-                   <img class="card-img-top object-fit-cover h-100" src="${data[i].thumbnail}" />
-                
-                </figure>
-    
-                <figcaption>
-    
-                   <div class="hstack justify-content-between">
-                      <h3 class="h6 small">${data[i].title}</h3>
-                      <span class="badge text-bg-primary p-2">Free</span>
-                   </div>
-    
-                   <p class="card-text small text-center opacity-50">
-                      ${data[i].short_description.split(" ", 8)}
-                   </p>
-    
-                </figcaption>
-             </div>
-    
-             <footer class="card-footer small hstack justify-content-between">
-    
-                <span class="badge badge-color">${data[i].genre}</span>
-                <span class="badge badge-color">${data[i].platform}</span>
-    
+   displayGamesData(data) {
+     let games = ``;
+     for (let i = 0; i < data.length; i++) {
+       games += `
+           <div class="col">
+           <div data-id="${
+         
+             data[i].id
+           }"  class="card h-100 bg-transparent" role="button" ">
+              <div  class="card-body">
+                 <figure class="position-relative">
+                    <img class="card-img-top object-fit-cover h-100" src="${
+                      data[i].thumbnail
+                    }" />                
+                 </figure>
+                 <caption>    
+                    <div class="hstack justify-content-between">
+                       <h3 class="h6 small">${data[i].title}</h3>
+                       <span class="badge text-bg-primary p-2">Free</span>
+                    </div>    
+                    <p class="card-text small text-center opacity-50">
+                       ${data[i].short_description.split(" ", 15)}
+                    </p>    
+                 <caption>
+              </div>
+              <footer class="card-footer small hstack justify-content-between">    
+                 <span class="badge badge-color">${data[i].genre}</span>
+                 <span class="badge badge-color">${data[i].platform}</span>    
              </footer>
-          </div>
-       </div>
-          `;
-       }
+           </div>
+        </div>
+           `;
+     }
+     document.getElementById("gameData").innerHTML = games;
+   }
  
-       document.getElementById("gameData").innerHTML = gamesBox;
-    }
- 
-    displayDetails(data) {
-        let screenshots = ``;
-       const content = `
+   displayDetails(data) {
+     let screenshots = ``;
+     const content = `
        <div class="col-md-4">
        <img src="${data.thumbnail}" class="w-100" alt="image details" />
     </div>
@@ -55,12 +51,11 @@ export class Ui {
     </div>
        
        `;
-       for (let i = 0; i < data.screenshots.length; i++) {
-        screenshots += `<div class="py-4 px-2"><img class="w-100 img-fluid object-fit-cover" src="${data.screenshots[i].image}" alt=""></div>`;
-      }
- 
-       document.getElementById("detailsContent").innerHTML = content;
-       document.getElementById("screenshots").innerHTML = screenshots;
-    }
+     for (let i = 0; i < data.screenshots.length; i++) {
+       screenshots += `<div class="py-4 px-2"><img class="w-100 img-fluid object-fit-cover" src="${data.screenshots[i].image}" alt=""></div>`;
+     }
+     document.getElementById("detailsContent").innerHTML = content;
+     document.getElementById("details-photo").innerHTML = screenshots;
+   }
  }
  
